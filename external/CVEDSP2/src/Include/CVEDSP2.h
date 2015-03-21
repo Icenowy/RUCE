4fa52ada60d22f1abbba75184036f27cd802db5e
Star Brilliant <m13253@hotmail.com>
2015-03-21 19:49:32 +0800

    Squashed 'external/CVESVP/' content from commit aaf72be
    git-subtree-dir: external/CVESVP
    git-subtree-split: aaf72be3eef2c070f2943cc9cf2c82cd12131c16

---
+++ external/CVEDSP2/src/Include/CVEDSP2.h
@@ -0,0 +1,11 @@
+#ifndef __CDSP2_Install
+#define __CDSP2_Install
+#endif
+
+#include <CVEDSP2/Interface/Setting.h>
+#include <CVEDSP2/Container/Wave.h>
+#include <CVEDSP2/Container/InfWave.h>
+#include <CVEDSP2/Container/Spectrum.h>
+#include <CVEDSP2/Container/DataFrame.h>
+#include <CVEDSP2/List/Lists.h>
+
---
+++ src/_VOT.h
@@ -0,0 +1,4 @@
+RTFunc(int, CSVP_VOTFromWave, void* Sorc, int LBound, int RBound);
+RTFunc(int, CSVP_OnsetFromWave, void* Sorc, _T1 Threshold, int LBound,
+    int RBound);
+
